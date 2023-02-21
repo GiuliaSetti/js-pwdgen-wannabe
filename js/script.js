edf6d0480b27a1e5b45ec2edd8bb38cb102254c8
GiuliaSetti <giulia.setti96@gmail.com>
2023-02-21 15:19:09 +0100

    JS : exercise completed - test

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina la concatenazione delle variabili + un numero a caso scelto da voi
+// (es: MarioRossiVerde21)
+
+
+/*Chiedi il nome all'utente */
+
+let userName = prompt("Qual è il tuo nome?");
+
+/* Chiedi il cognome all'utente */
+
+let userSurname = prompt("Qual è il tuo cognome?");
+
+/* Chiedi all'utente il suo colore preferito */
+
+let userFavColor = prompt("Qual è il tuo colore preferito?");
+
+/* Visualizza le risposte su pagina, con numero a caso */
+
+document.writeln(userName + userSurname + userFavColor + "06");
